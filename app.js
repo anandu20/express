ad82dda7076af4822aa07b49f76d0229db4b89c7
anandu20 <ananduramachnadran411@gmail.com>
2024-09-10 09:06:49 +0530

    mmm

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
-const bodyParser=require("body-parser")
+const path=require("path")
 const express=require("express")
 const app=express();
 const PORT=3000;
-
-app.use(bodyParser.urlencoded())
-app.post("/",(req,res)=>{
-    res.status(200).send("hello world")
+// app.use(express.static("template")) // used to load a page without using get methd
+// app.use(express.json())   // load a page without using body parser
+app.get("/",(req,res)=>{
+    console.log(req.body);
+    res.status(200).sendFile(path.join(__dirname,"template","index.html"));
+    
+  
 })
+app.get("/about",(req,res)=>{
+   
+
+    res.status(201).sendFile(path.join(__dirname,"template","about.html"));
+
+})
+
+app.get("/contact",(req,res)=>{
+
+    res.status(202).sendFile(path.join(__dirname,"template","contact.html"));
+   
+   })
+   
+
+app.get("*",(req,res)=>{
+
+ res.status(404).sendFile(path.join(__dirname,"template","error.html"));
+
+})
+// app.get("/",Token,(req,res,next)=>{
+//     console.log("home");
+//     res.send("hello World");
+        
+
+// })
+// function Token(req,res,next){
+//     console.log("middle ware");
+//     next();
+// }
+
+
 app.listen(PORT,()=>{
     console.log(`server started at http://localhost:${PORT}`);
     
